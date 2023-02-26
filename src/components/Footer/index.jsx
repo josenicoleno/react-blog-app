fd4d6ecf050a71994cf0bcbaecfc263d99042018
Jose Nicoleno <josenicoleno@hotmail.com>
2023-02-26 16:54:14 +0100

    footer fix

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <div className="footer-content">
                 <ul className="socials">
-                    <li><a href="https://www.fb.com/josenicoleno"><i className="fa fa-facebook"></i></a></li>
-                    <li><a href="https://www.instagram.com/jose.nicoleno/"><i className="fa fa-instagram"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/jose-nicoleno/"><i className="fa fa-linkedin"></i></a></li>
-                    <li><a href="https://github.com/josenicoleno"><i className="fa fa-github"></i></a></li>
+                    <li><a href="https://www.fb.com/josenicoleno" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook"></i></a></li>
+                    <li><a href="https://www.instagram.com/jose.nicoleno/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/jose-nicoleno/" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a></li>
+                    <li><a href="https://github.com/josenicoleno" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a></li>
                 </ul>
                 <h3>José Nicoleno</h3>
                 <p>Este sitio lo realicé con JavaSript, React, Bootstrap y otras dependencias. Para más información ponte en contacto conmigo por cualquiera de mis plataformas o escríbeme a <a href="mailto:josenicoleno@gmail.com">josenicoleno@gmail.com</a></p>
